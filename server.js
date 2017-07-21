8edfe93959f2507ba897f197f060cb343070fafe
chesteraustin <chesteraustin@gmail.com>
2017-07-21 11:35:09 -0700

    add PUBLIC as static folder

--- server.js
+++ server.js
@@ -14,6 +14,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.text());
 app.use(bodyParser.json({ type: "application/vnd.api+json" }));
+app.use(express.static('public'))
 
 // Routes
 require('./app/routing/apiRoutes')(app);
