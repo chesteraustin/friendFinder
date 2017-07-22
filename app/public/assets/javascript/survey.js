dc43d36ec8de96df764b93349a4964fa71dcd30e
chesteraustin <chesteraustin@gmail.com>
2017-07-21 19:36:20 -0700

    stubs for SURVEY question

---
+++ app/public/assets/javascript/survey.js
@@ -0,0 +1,30 @@
+$( document ).ready(function(){
+	$("#subForm").on("click", function(){
+		//Prevent Submission
+		event.preventDefault();
+
+		var formResult = [{
+							"name":"Peter Parker",
+							"photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+							"scores":[0,0,0,0,0,0,0,0,0,0]
+							}];
+
+			$.ajax({
+				url: '/api/friends',
+				dataType: 'json',
+				type: 'post',
+				data: {
+					"name": "Clark Kent",
+					"photo": "photo link",
+					"scores": [0,0,0,0,0,0,0,0,0,0]
+				},
+				error: function(error) {
+					console.log(error)
+				},
+				success: function(data) {
+					console.log(data)
+				}
+			});
+
+	})
+})
