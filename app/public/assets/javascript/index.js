136b2df20c489ba5db7bf5bc942a7f5226bac7de
chesteraustin <chesteraustin@gmail.com>
2017-07-21 23:20:16 -0700

    add datatables to display saved friends

--- app/public/assets/javascript/index.js
+++ app/public/assets/javascript/index.js
@@ -1,3 +1,20 @@
 $( document ).ready(function(){
 	console.log("document loaded")
+
+	var table = $('#table').dataTable( {
+		"ajax": {
+			"url": "api/friends",
+			"dataSrc": "",
+			"type": "get"
+		},
+		"columns": [
+			{ "data": "name" },
+			{ "data": "photo" }
+		]
+	});
+
+	$.get("api/friends", function( data ) {
+		console.log(data);
+		$("#friendsJSON").text(JSON.stringify(data))
+	});
 })
